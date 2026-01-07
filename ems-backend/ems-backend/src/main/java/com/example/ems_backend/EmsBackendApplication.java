a9f29d0e15e373bfc1affaa2feb9bdd33f1d892f
Sankeerth-Digitus <sankeerth.bd.ext@digitusbiz.com>
2026-01-07 12:47:56 +0530

    Verified with the new Branch

--- ems-backend/ems-backend/src/main/java/com/example/ems_backend/EmsBackendApplication.java
+++ ems-backend/ems-backend/src/main/java/com/example/ems_backend/EmsBackendApplication.java
@@ -2,15 +2,12 @@ package com.example.ems_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class EmsBackendApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context=SpringApplication.run(EmsBackendApplication.class, args);
-		Project obj=context.getBean(Project.class);
-		obj.show();
+		SpringApplication.run(EmsBackendApplication.class, args);
 	}
 
 }
